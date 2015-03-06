fb4f20ed50151c98d41382ba7a43af09171df60e
gyokay <gyokayakmed@gmail.com>
2015-03-06 15:03:34 +0200

    added ne homeworks

---
+++ CSharp2/Strings and Text Processing/Problem2.ReverseString/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "dog";
+            char[] charArr = inputString.ToCharArray();
+            Array.Reverse(charArr);
+            Console.WriteLine(charArr);
+        }
+    }
+}
---
+++ CSharp2/Strings and Text Processing/SumOfaString/SumOfAString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string asd = "12 32 2 32";
+        string[] txt = asd.Split(' ');
+        int sum = 0;
+        for (int i = 0; i < txt.Length; i++)
+        {
+          sum += int.Parse(txt[i]);
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
