42f0460fb00020ed28a987508b3264ef961d70ab
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-15 22:29:53 +0300

    Update 09. Point Circle Rectangle

--- JS-Fundamentals-Homeworks/Operators-and-Expressions/09.PointCircleRectangle.js
+++ JS-Fundamentals-Homeworks/Operators-and-Expressions/09.PointCircleRectangle.js
@@ -6,5 +6,26 @@ function solve(argument) {
     var circleY = 1;
     var circleR = 1.5;
 
-    var rectangleX = 1;
+    var rectangleX = -1;
+    var rectangleX1 = -1 + 6;
+    var rectangleY = 1;
+    var rectangleY1 = 1 - 2;
+
+    var isInCircle = Math.pow(x - circleX, 2) + Math.pow(y - circleY, 2) <= Math.pow(circleR, 2);
+
+    var output = "";
+
+    if (isInCircle) {
+        output += "inside circle ";
+    } else {
+        output += "outside circle ";
+    }
+
+    if (((x >= rectangleX) && (x <= rectangleX1)) && ((y <= rectangleY) && (y >= rectangleY1))) {
+        output += "inside rectangle";
+    } else {
+        output += "outside rectangle";
+    }
+
+    return output;
 }
