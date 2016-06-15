3a79783d86b4ecb0945df9e97f20b23658faee1a
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-15 21:59:32 +0300

    Solve problem 09. Point, Circle, Rectangle

---
+++ JS-Fundamentals-Homeworks/Operators-and-Expressions/09.PointCircleRectangle.js
@@ -0,0 +1,10 @@
+function solve(argument) {
+    var x = Number(argument[0]);
+    var y = Number(argument[1]);
+
+    var circleX = 1;
+    var circleY = 1;
+    var circleR = 1.5;
+
+    var rectangleX = 1;
+}
