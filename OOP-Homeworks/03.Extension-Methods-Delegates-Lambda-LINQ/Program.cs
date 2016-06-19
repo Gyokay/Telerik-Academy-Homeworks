f103d995cb67683681b30016e02a5bbf360193cf
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-19 21:46:34 +0300

    Update homework

--- OOP-Homeworks/03.Extension-Methods-Delegates-Lambda-LINQ/Program.cs
+++ OOP-Homeworks/03.Extension-Methods-Delegates-Lambda-LINQ/Program.cs
@@ -13,9 +13,9 @@ namespace _03.Extension_Methods_Delegates_Lambda_LINQ
 
             Student[] students = new Student[3]
             {
-                new Student { FirstName = "Ivan", LastName = "Ivanov" },
-                new Student { FirstName = "Ivanka", LastName = "Ivanova" },
-                new Student { FirstName = "Georgi", LastName = "Georgiev" }
+                new Student { FirstName = "Ivan", LastName = "Ivanov", Age = 22 },
+                new Student { FirstName = "Ivanka", LastName = "Ivanova", Age = 23 },
+                new Student { FirstName = "Georgi", LastName = "Georgiev", Age = 99 }
 
             };
         }
@@ -26,13 +26,26 @@ namespace _03.Extension_Methods_Delegates_Lambda_LINQ
         //all students whose first name is before its last name alphabetically.
         //Use LINQ query operators.
 
-        public static Student[] FilterStudents(Student[] students)
+        public static Student[] FilterStudentsName(Student[] students)
         {
             Student[] filteredStudents = (from student in students
                                           where student.FirstName.CompareTo(student.LastName) < 0
                                           select student).ToArray();
 
             return filteredStudents;
+        }
+
+        //Problem 4. Age range
+        //Write a LINQ query that finds the first name and last 
+        //name of all students with age between 18 and 24.
+
+        public static Student[] FilterStudentsAge(Student[] students)
+        {
+            Student[] studentsFilteredByAge = (from student in students
+                                               where student.Age >= 18 && student.Age <= 24
+                                               select student).ToArray();
+
+            return studentsFilteredByAge;
         }
     }
 }
--- OOP-Homeworks/03.Extension-Methods-Delegates-Lambda-LINQ/Student.cs
+++ OOP-Homeworks/03.Extension-Methods-Delegates-Lambda-LINQ/Student.cs
@@ -4,5 +4,6 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public byte Age { get; set; }
     }
 }
