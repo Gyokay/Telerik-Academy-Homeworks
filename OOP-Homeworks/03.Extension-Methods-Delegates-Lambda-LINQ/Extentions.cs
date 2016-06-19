296e93ac7dc4f1d9e02aa15bd66606ad0aa48b65
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-19 17:51:51 +0300

    Start homework Extension-Methods-Delegates-Lambda-LINQ

---
+++ OOP-Homeworks/03.Extension-Methods-Delegates-Lambda-LINQ/Extentions.cs
@@ -0,0 +1,88 @@
+﻿namespace _03.Extension_Methods_Delegates_Lambda_LINQ
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class Extentions
+    {
+
+        //Problem 1. StringBuilder.Substring
+        //Implement an extension method  Substring(int index, int length)
+        //for the class  StringBuilder  that returns new  StringBuilder  
+        //and has the same functionality as  Substring  in the class  String .
+        public static StringBuilder Substring(this StringBuilder stringBuilder, int index, int length)
+        {
+            var strBuilder = new StringBuilder();
+
+            for (int i = index; i < length; i++)
+            {
+                strBuilder.Append(stringBuilder[i]);
+            }
+
+            return strBuilder;
+        }
+
+        //Problem 2. IEnumerable extensions
+        //Implement a set of extension methods for  IEnumerable<T> that implement 
+        //the following group functions: sum, product, min, max, average.
+        public static T Sum<T>(this IEnumerable<T> collection)
+            where T : IComparable
+        {
+            dynamic sum = default(T);
+
+            foreach (T item in collection)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static T Product<T>(this IEnumerable<T> collection)
+        {
+            dynamic prod = 1;
+
+            foreach (T item in collection)
+            {
+                prod *= item;
+            }
+
+            return prod;
+        }
+
+        public static T Min<T>(this IEnumerable<T> collection)
+            where T : IComparable
+        {
+            T min = collection.First();
+
+            foreach (T item in collection)
+            {
+                if (item.CompareTo(min) < 0)
+                {
+                    min = item;
+                }
+            }
+
+            return min;
+        }
+
+        public static T Max<T>(this IEnumerable<T> collection)
+            where T : IComparable
+        {
+            T max = collection.First();
+
+            foreach (var item in collection)
+            {
+                if (item.CompareTo(max) > 0)
+                {
+                    max = item;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ OOP-Homeworks/03.Extension-Methods-Delegates-Lambda-LINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Extension_Methods_Delegates_Lambda_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new StringBuilder();
+            foo.Append("Problem 1. StringBuilder.Substring");
+
+            var bar = foo.Substring(0, 3);
+
+            string fomeString = "some string";
+            string someNewString = "some new string";
+
+            var sum = someNewString.Sum();
+
+            Console.WriteLine(sum);
+        }
+    }
+}
