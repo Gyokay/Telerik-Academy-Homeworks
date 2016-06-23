837bb5f1018cfb6aeec68f711b2835821a6539b9
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-23 10:51:14 +0300

    Solve 08. Number as words

--- JS-Fundamentals-Homeworks/Conditional-Statements/08.NumberAsWords.js
+++ JS-Fundamentals-Homeworks/Conditional-Statements/08.NumberAsWords.js
@@ -1,115 +1,173 @@
-function solve(input) {
-    'use strict';
+function solve(args) {
+    "use strict";
 
-    var number = Number(input[0]);
+    var input = String(args);
 
-    function getDigitsFromZeroToNine(number) {
-        switch (number) {
-            case 0:
-                return 'zero';
-            case 1:
-                return 'one';
-            case 2:
-                return 'two';
-            case 3:
-                return 'three';
-            case 4:
-                return 'four';
-            case 5:
-                return 'five';
-            case 6:
-                return 'six';
-            case 7:
-                return 'seven';
-            case 8:
-                return 'eight';
-            case 9:
-                return 'nine';
+    var length = input.length;
+    var output = "";
+    var isTeen = false;
+
+    if (length == 3) {
+        var digit = input.charAt(0);
+
+        switch (digit) {
+            case "1":
+                output += "one hundred";
+                break;
+            case "2":
+                output += "two hundred";
+                break;
+            case "3":
+                output += "three hundred";
+                break;
+            case "4":
+                output += "four hundred";
+                break;
+            case "5":
+                output += "five hundred";
+                break;
+            case "6":
+                output += "six hundred";
+                break;
+            case "7":
+                output += "seven hundred";
+                break;
+            case "8":
+                output += "eight hundred";
+                break;
+            case "9":
+                output += "nine hundred";
+                break;
         }
+
+        input = input.substr(1, 2);
     }
 
-    function getNumbersFromTenToNineteen(number) {
-        switch (number) {
-            case 10:
-                return 'ten';
-            case 11:
-                return 'eleven';
-            case 12:
-                return 'twelve';
-            case 13:
-                return 'thirteen';
-            case 14:
-                return 'fourteen';
-            case 15:
-                return 'fifteen';
-            case 16:
-                return 'sixteen';
-            case 17:
-                return 'seventeen';
-            case 18:
-                return 'eighteen';
-            case 19:
-                return 'nineteen';
-        }
-    }
+    if (length >= 2) {
 
-    function getNumbersFromTwentyToNinety(number) {
-        switch (number) {
-            case 2:
-                return 'twenty';
-            case 3:
-                return 'thirty';
-            case 4:
-                return 'forty';
-            case 5:
-                return 'fifty';
-            case 6:
-                return 'sixty';
-            case 7:
-                return 'seventy';
-            case 8:
-                return 'eighty';
-            case 9:
-                return 'ninety';
-        }
-    }
+        var digit = input.substring(0, 1);
 
-    function getNumberAsWords() {
-        var result = '';
-
-        if (number >= 100) {
-            result += getDigitsFromZeroToNine(Math.floor(number / 100));
-            result += ' hundred and ';
-            number %= 100;
-        }
-        if (number > 19) {
-            result += getNumbersFromTwentyToNinety(Math.floor(number / 10));
-            result += ' ';
-            number %= 10;
-        } else if (number > 9) {
-            result += getNumbersFromTenToNineteen(number);
-            result += ' ';
-            number = 0;
-        }
-        if (number > 0) {
-            result += getDigitsFromZeroToNine(number);
-            result += ' ';
-            number = 0;
-        }
-        if (number === 0) {
-            if (result.length === 0) {
-                result += 'zero';
+        if (digit === '1') {
+            if (length === 3) {
+                output += " and ";
+            }
+            isTeen = true;
+            var digit = input.substring(0, 2);
+            switch (digit) {
+                case "10":
+                    output += "ten";
+                    break;
+                case "11":
+                    output += "eleven";
+                    break;
+                case "12":
+                    output += "twelve";
+                    break;
+                case "13":
+                    output += "thirteen";
+                    break;
+                case "14":
+                    output += "fourteen";
+                    break;
+                case "15":
+                    output += "fifteen";
+                    break;
+                case "16":
+                    output += "sixteen";
+                    break;
+                case "17":
+                    output += "seventeen";
+                    break;
+                case "18":
+                    output += "eigthteen";
+                    break;
+                case "19":
+                    output += "nineteen";
+                    break;
             }
         }
+        else {
+            if (length == 3 && input[1] !== '0')  {
+                output += " and ";
+            }
+            digit = input.substring(0, 1);
 
-        return result;
+            switch (digit) {
+                case "2":
+                    output += "twenty";
+                    break;
+                case "3":
+                    output += "thirty";
+                    break;
+                case "4":
+                    output += "forty";
+                    break;
+                case "5":
+                    output += "fifty";
+                    break;
+                case "6":
+                    output += "sixty";
+                    break;
+                case "7":
+                    output += "seventy";
+                    break;
+                case "8":
+                    output += "eighty";
+                    break;
+                case "9":
+                    output += "ninety";
+                    break;
+            }
+
+            input = input.substr(1);
+        }
     }
 
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+    if (!isTeen) {
+        var digit = input.substr(0);
+        switch (digit) {
+            case "0":
+                if (length == 1) {
+                    output += "zero";
+                }
+                break;
+            case "1":
+                output += " one";
+                break;
+            case "2":
+                output += " two";
+                break;
+            case "3":
+                output += " three";
+                break;
+            case "4":
+                output += " four";
+                break;
+            case "5":
+                output += " five";
+                break;
+            case "6":
+                output += " six";
+                break;
+            case "7":
+                output += " seven";
+                break;
+            case "8":
+                output += " eight";
+                break;
+            case "9":
+                output += " nine";
+                break;
+        }
     }
 
-    var result = capitalizeFirstLetter(getNumberAsWords(number));
+    var replacespaces = new RegExp('  ', 'g');
 
-    console.log(result);
+    output = output.trim();
+
+    var firstChar = output[0];
+    firstChar = firstChar.toUpperCase();
+    var print = firstChar + output.slice(1);
+    print = print.replace(replacespaces, ' ');
+    console.log(print);
 }
