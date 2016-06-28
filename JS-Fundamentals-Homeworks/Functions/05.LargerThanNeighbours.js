603155b28cb9984c2fa602798f724c783f91fe76
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-28 11:46:50 +0300

    Solve Larger than neighbours

---
+++ JS-Fundamentals-Homeworks/Functions/05.LargerThanNeighbours.js
@@ -0,0 +1,15 @@
+    function solve(args) {
+        'use strict';
+
+        var input = args[0].split('\n');
+        var arr = input[1].split(' ').map(Number);
+        var counter = 0;
+        for (var i = 1; i < arr.length - 1; i += 1) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                counter += 1;
+            }
+        }
+
+        return counter;
+
+    }
